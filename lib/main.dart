52e47e6e168754d25167168bad35759444555aa5
nhhfidu5rur <93320383+nhhfidu5rur@users.noreply.github.com>
2026-09-04 08:25:12 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Excel Manager',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1E3A5F),
+          brightness: Brightness.light,
+        ),
+        textTheme: GoogleFonts.cairoTextTheme(),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
